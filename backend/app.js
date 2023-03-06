184f42b8d7b43797aadbcdbdb059942db931d1a0
Eline31 <eline.chambon@gmail.com>
2023-03-06 17:34:00 +0100

    configuration post et get

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,48 @@ const express = require("express");
 //Appel de la méthode express
 const app = express();
 
+//Middleware qui intercepte les req qui ont un content-type json, permet d'accéder au body de la requête, comme bodyparser
+app.use(express.json());
+
+//Ajout d'un middleware général (appliqué à toutes les routes) pour parer à l'erreur CORS
 app.use((req, res, next) => {
-    console.log("Requête reçue !");
+    //accéder à notre API depuis n'importe quelle origine *
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //ajouter les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    //envoyer des requêtes avec les méthodes mentionnées
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
+app.post("/api/stuff", (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: "Objet créé"
+    });
 })
 
-app.use((req, res, next) => {
-    res.json({ message: "Votre requêtre a bien été reçue !" });
-    next();
-});
-
-app.use((req, res) => {
-    console.log("Réponse envoyée avec succès !");
+///api/stuff est l'url visée par l'application, la route sur notre API
+app.get("/api/stuff", (req, res, next) => {
+    const stuff = [
+        {
+            _id: "fkepokf",
+            title: "Mon premier objet",
+            description: "Les infos de mon premier objet",
+            imageUrl: "",
+            price: 4900,
+            userId: "kpfrpkf",
+        },
+        {
+            _id: "jfeozgg",
+            title: "Mon deuxième objet",
+            description: "Les infos de mon deuxième objet",
+            imageUrl: "",
+            price: 2900,
+            userId: "joivzjv",
+        },
+    ];
+    res.status(200).json(stuff);
 });
 
 //Export d'app pour pouvoir l'utiliser ailleurs
