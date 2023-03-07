2e0c41e3be44bf9d5eb6e9b04e30bc8242800593
Eline31 <eline.chambon@gmail.com>
2023-03-07 19:26:53 +0100

    Implémentation middleware authentification

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const auth = require("auth");
 const router = express.Router();
+
 
 const stuffCtrl = require("../controllers/stuff");
 
-router.post("/", stuffCtrl.createThing);
-router.put("/:id", stuffCtrl.modifyThing);
-router.delete("/:id", stuffCtrl.deleteThing);
-//le :id sinifique que cette partie de la route est dynamique
-router.get("/:id", stuffCtrl.getOneThing);
 ///api/stuff est l'url visée par l'application, la route sur notre API
-router.get("/", stuffCtrl.getAllThings);
+router.get("/", auth, stuffCtrl.getAllThings);
+router.post("/", auth, stuffCtrl.createThing);
+router.put("/:id", auth, stuffCtrl.modifyThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+//le :id sinifique que cette partie de la route est dynamique
+router.get("/:id", auth, stuffCtrl.getOneThing);
+
 
 module.exports = router;
