0a9df76e7741705eee62ea2a05197971757e854e
Eline31 <eline.chambon@gmail.com>
2023-03-07 18:33:39 +0100

    Création utilisateur

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,20 @@
-exports.signup = (req, res, next) => {
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
+exports.signup = (req, res, next) => {
+    //10 représente le nombre de tours de hachage
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+                .catch(error => res.status(400).json({ error }))
+        })
+        //erreur 500 = erreur de serveur
+        .catch(error => res.status(500).json({ error }))
 };
 
 exports.login = (req, res, next) => {
