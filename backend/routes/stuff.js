be792d031d346c61082ab292550ecb05b9d30f24
Eline31 <eline.chambon@gmail.com>
2023-03-08 11:40:41 +0100

    Modification route PUT

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -9,10 +9,11 @@ const stuffCtrl = require("../controllers/stuff");
 ///api/stuff est l'url visée par l'application, la route sur notre API
 router.get("/", auth, stuffCtrl.getAllThings);
 router.post("/", auth, multer, stuffCtrl.createThing);
-router.put("/:id", auth, stuffCtrl.modifyThing);
-router.delete("/:id", auth, stuffCtrl.deleteThing);
 //le :id sinifique que cette partie de la route est dynamique
 router.get("/:id", auth, stuffCtrl.getOneThing);
+router.put("/:id", auth, multer, stuffCtrl.modifyThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+
 
 
 module.exports = router;
